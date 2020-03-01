546b44e81750b1b20ef873de947a3626b7d7e8b5
Lesly Juarez <leslyj2@gmail.com>
2020-03-01 20:53:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do This
 
-def fizzbuzz
-end
-
-def fizzBuzz(number)
+def fizzbuzz(number)
   if number % 3 && number % 5
     "FizzBuzz"
   elsif number % 3 
